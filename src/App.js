a88c2829dbe88d3cbc59084a39ee236883fd7ac5
ls <austin.sefton@gmail.com>
2018-05-17 18:33:41 +0000

    toolbar added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import {Grid, Row, Col} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
+import {AppBar, Toolbar} from '@material-ui/core';
+
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <AppBar position="static">
+                    <Toolbar />
+                </AppBar>
                 <div className="container">
                     <Grid>
                         <Row>
